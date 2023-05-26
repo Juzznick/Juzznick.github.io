36a088f27266194b5fb53c11a046e64a6da9e0bf
Juzznick <79796884+Juzznick@users.noreply.github.com>
2023-05-26 11:44:10 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
+const card_observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if(entry.isIntersecting) {
+            entry.target.classList.add('cards_show');
+        } else{
+            entry.target.classList.remove('cards_show');
+        }
+    });
+});
+
+const hiddenCards = document.querySelectorAll('.cards_hidden');
+hiddenCards.forEach((el) => card_observer.observe(el));
