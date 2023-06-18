41b7f2199f2bccb7a76aca2e777495c59b7f3a66
Juzznick <xnick126@gmail.com>
2023-06-18 19:40:38 +0530

    added some minor hidden/show stuff on the percentrack site

--- percentrack/main.js
+++ percentrack/main.js
@@ -2,20 +2,27 @@ var pr= document.getElementById("present_count");
 var ab= document.getElementById("absent_count");
 var res= document.getElementById("result");
 var lef= document.getElementById("left");
+const Div = document.getElementById("div_left");
+Div.style.display="none";
 
 document.getElementById("compute").addEventListener("click", function(){
     res.value=   (parseInt(pr.value)/(parseInt(pr.value)+parseInt(ab.value)) * 100).toFixed(2) + "%" ;
+    lef.value=null;
     if(parseInt(res.value)<75 && parseInt(res.value)!=null){
-
         var toattend= (parseInt(ab.value)*3);
         var left = (parseInt(toattend)-parseInt(pr.value));
         lef.value= left;
         
     }
+    if(left != null){
+        Div.style.display = "block";
+    }
+    else{
+        Div.style.display = "none";
+    }
+
+    
 });
-
-
-
 
 
 
